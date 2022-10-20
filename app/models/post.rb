b3b277e40659b43a4cc359b99c09d11f19fb947e
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-10-20 15:18:31 +0300

    Add posts using Post.create

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :Comments
+    belongs_to :user, index: { unique: true }, foreign_key: true
+    has_many :comments
     has_many :likes
 end
