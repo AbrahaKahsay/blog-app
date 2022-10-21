b7769b5bd680b0b3bb40b5674dfc3b0ed38796ea
AbrahaKahsay <abrahak81@gmail.com>
2022-10-21 16:13:05 +0300

    Add tests to comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Comment.new(post: @post, user: @user, text: 'Hi Tom!')
+  end
+
+  before { subject.save }
+
+  it 'should ensure text presence' do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'text must not exceed 255 characters' do
+    subject.text = 'a' * 300
+    expect(subject).to_not be_valid
+  end
+
+  it 'post should be present' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'user should be present' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
 end
