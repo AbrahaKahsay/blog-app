052900846c106ca8db0acb4d65358b36bcb23396
AbrahaKahsay <abrahak81@gmail.com>
2022-10-21 15:24:27 +0300

    Add unit tests to like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Like.new(user: @user, post: @post)
+  end
+
+  before { subject.save }
+
+  it 'post should be present' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+  it 'usser should be present' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
 end
