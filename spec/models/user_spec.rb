68252887ef5612c4444146a08525feb1cab70f88
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-10-21 13:12:34 +0300

    Add unit tests to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Abraha', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer from Ethiopia.') }
+  before { subject.save }
+
+  it 'should ensure name presence' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should return valid for posts greater or equla to 0' do
+    subject.posts_counter = 2
+    expect(subject).to be_valid
+  end
 end
