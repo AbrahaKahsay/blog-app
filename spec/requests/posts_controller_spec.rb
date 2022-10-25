026640871caeb69bd174bb4a13a1973ba69c6485
AbrahaKahsay <abrahak81@gmail.com>
2022-10-25 22:15:32 +0300

    Add request spects with body response

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "PostsControllers", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) { get user_posts_path(745) }
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it "assigns all users to @posts" do
+      expect(assigns(:posts)).to eq(Post.all)
+    end
+
+    it 'matches the placeholder text with the respone body' do
+      expect(response.body).to include 'Here is a list of posts for a given user name'
+    end
   end
 end
