921b8a2dd6aea3dbf5c94160493b344dd4d46ead
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-10-25 10:32:21 +0300

    Create posts controller and modify users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,9 @@
 class UsersController < ApplicationController
-
+    
     def index
         @users = User.all
     end
 
-    def new
-        @user = User.new
-    end
-
-    def create
-        @user = User.create(user_params)
-        redirect_to @user
-    end
-
     def show
-    end
-
-    private
-    def user_params
-        params.require(:user).permit(:name, :photo, :bio)
     end
 end
