c63ae356d3bff3231962d5cb421d861000a212d2
AbrahaKahsay <abrahak81@gmail.com>
2022-10-25 21:01:26 +0300

    Add request spect to users

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "UsersControllers", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) { get users_path }
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
