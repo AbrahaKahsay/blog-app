d00b5d7d432ca7d6e07b047542280c13b4b5ba7b
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-10-25 21:23:02 +0300

    Add renders and assigns to users

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -3,8 +3,17 @@ require 'rails_helper'
 RSpec.describe "UsersControllers", type: :request do
   describe "GET /index" do
     before(:each) { get users_path }
+
     it "is a success" do
       expect(response).to have_http_status(:ok)
     end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it "assigns all users to @users" do
+      expect(assigns(:users)).to eq(User.all)
+     end
   end
 end
