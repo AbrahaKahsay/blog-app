3c2baf9bb34614d0e14ed97782a9472e4828443b
AbrahaKahsay <abrahak81@gmail.com>
2022-10-25 22:29:37 +0300

    Add specs for show action of users and posts

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "UsersControllers", type: :request do
-  describe "GET /index" do
+RSpec.describe 'UsersControllers', type: :request do
+  describe 'GET /index' do
     before(:each) { get users_path }
 
-    it "is a success" do
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
@@ -12,12 +12,26 @@ RSpec.describe "UsersControllers", type: :request do
       expect(response).to render_template('index')
     end
 
-    it "assigns all users to @users" do
+    it 'assigns all users to @users' do
       expect(assigns(:users)).to eq(User.all)
-     end
+    end
 
-     it 'matches the placeholder text with the respone body' do
+    it 'matches the placeholder text with the respone body' do
       expect(response.body).to include 'Here is a list of users with name'
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) { get user_path(1) }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
+    end
+    it 'matches the placeholder text with the respone body' do
+      expect(response.body).to include 'Here is a list of users with given id'
     end
   end
 end
