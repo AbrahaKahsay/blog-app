dd565616b2fb097d29499a085409504489c84f15
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-10-28 21:31:16 +0300

    Add new method to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(@post.user_id)
   end
+
+  def new
+    @post = Post.new
+  end
+  
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
