a6965dadb3da57d101581092618a127ee8d2b35b
AbrahaKahsay <abrahak81@gmail.com>
2022-10-29 09:06:02 +0300

    Finish working on tasks

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,32 +4,36 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def new
+    @current_user = current_user
+    @post = Post.new
+    @user = User.find(params[:user_id].to_i)
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    @user = current_user
+
+    if @post.save
+      redirect_to user_post_path(@user, @post)
+      flash[:error] = 'Post successfuly created!'
+    else
+      flash[:error] = 'Error creating post'
+      render 'new'
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
     @user = User.find(@post.user_id)
   end
 
-  def new
-    @post = Post.new
-    @user = current_user
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
-    if @post.save
-      flash[:success] = "Post successfully created"
-      redirect_to user_posts_path(current_user.id)
-    else
-      flash[:error] = "Something went wrong"
-      redirect_to new_user_post_path
-    end
-  end
-  
-
   private
 
   def post_params
-    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+    params.require(:post).permit(:title, :text)
   end
 end
