66118c80949b3bdcb38112957a360a9b97b22090
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-01 19:52:03 +0000

    add before each to describe index

--- spec/views/user_index_page.rb
+++ spec/views/user_index_page.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'User test', type: :system do
-  describe 'check the user page to have user name' do
+  describe 'index' do
+    before(:each) do
+      @user1 = User.create(name: 'Tom', photo: 'https://www.google.com/image/1', bio: 'He is Abrahas friend', posts_counter: 2)
+      @user2  = User.create(name: 'Youssef', photo: 'https://www.google.com/image/4', bio: 'He is Abrahas best friend', posts_counter: 4)
+      visit '/'
+    end
+
     it 'shows the right content' do
       visit('/users')
       expect(page).to have_content('Abraha')
     end
+
+    it
+
+
   end
 end
